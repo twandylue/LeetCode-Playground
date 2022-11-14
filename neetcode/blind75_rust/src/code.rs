3598c9af12d5f345a12c31a11f5f9244ec56a6eb
AndyLu <laserlue@gmail.com>
2022-11-14 10:45:02 +0800

    init of product_of_array_except_self

--- neetcode/blind75_rust/src/code.rs
+++ neetcode/blind75_rust/src/code.rs
@@ -1,5 +1,6 @@
 pub mod contains_duplicate;
 pub mod group_anagrams;
+pub mod product_of_array_except_self;
 pub mod top_k_frequent_elements;
 pub mod two_sum;
 pub mod valid_anagram;
---
+++ neetcode/blind75_rust/src/code/product_of_array_except_self.rs
@@ -0,0 +1,11 @@
+use std::collections::HashMap;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {}
+
+    pub fn tests() {
+        let input = vec![1, 2, 3, 4];
+    }
+}
