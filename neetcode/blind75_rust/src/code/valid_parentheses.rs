2d07d41182d58d31e77759c9aaccfc30c1b482bc
AndyLu <laserlue@gmail.com>
2022-11-15 11:05:02 +0800

    complete valid_parentheses

--- neetcode/blind75_rust/src/code/valid_parentheses.rs
+++ neetcode/blind75_rust/src/code/valid_parentheses.rs
@@ -5,14 +5,28 @@ pub struct Solution {}
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut stack: Vec<char> = Vec::new();
-        let tuples = [("]", "["), ("}", "{"), (")", "(")];
-        let map: HashMap<&str, &str> = tuples.into_iter().collect();
+        let map = HashMap::from([(']', '['), ('}', '{'), (')', '(')]);
+        let target: Vec<char> = s.chars().collect();
 
-        s.chars().for_each(|x| {
-            // TODO:
-            // if map.contains_key(x) {}
-            stack.push(x);
-        });
+        for i in 0..target.len() {
+            if map.contains_key(&target[i]) {
+                if stack.len() == 0 {
+                    return false;
+                } else {
+                    let p = stack.pop().unwrap();
+                    let a = map.get(&target[i]).unwrap();
+                    if p != *a {
+                        return false;
+                    }
+                }
+            } else {
+                stack.push(target[i]);
+            }
+        }
+
+        if stack.len() != 0 {
+            return false;
+        }
 
         return true;
     }
@@ -20,11 +34,20 @@ impl Solution {
     pub fn tests() {
         let s = "()".to_string();
         let expected = true;
+        let actual = Solution::is_valid(s);
+
+        assert_eq!(actual, expected);
 
         let s2 = "()[]{}".to_string();
         let expected2 = true;
+        let actual2 = Solution::is_valid(s2);
+
+        assert_eq!(actual2, expected2);
 
         let s3 = "(]".to_string();
         let expected3 = false;
+        let actual3 = Solution::is_valid(s3);
+
+        assert_eq!(actual3, expected3);
     }
 }
