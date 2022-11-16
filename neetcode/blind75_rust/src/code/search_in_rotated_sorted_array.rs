3be8f088a470155be6c52f5f9b73b99eeaf2c561
AndyLu <laserlue@gmail.com>
2022-11-16 11:57:54 +0800

    complete search_in_rotated_sorted_array

--- neetcode/blind75_rust/src/code/search_in_rotated_sorted_array.rs
+++ neetcode/blind75_rust/src/code/search_in_rotated_sorted_array.rs
@@ -5,25 +5,45 @@ impl Solution {
         let mut l = 0;
         let mut r = nums.len() - 1;
 
-        while l < r {
+        while l <= r {
             let mid = (l + r) / 2;
             if nums[mid] == target {
                 return mid as i32;
             }
 
-            // TODO:
+            if nums[l] <= nums[mid] {
+                // left part
+                if nums[mid] < target || target < nums[l] {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            } else {
+                // left part
+                if nums[mid] > target || target > nums[r] {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
         }
 
-        return 0;
+        return -1;
     }
 
     pub fn tests() {
         let nums = vec![4, 5, 6, 7, 0, 1, 2];
         let target = 0;
         let expected = 4;
+        let actual = Solution::search(nums, target);
+
+        assert_eq!(actual, expected);
 
         let nums2 = vec![4, 5, 6, 7, 0, 1, 2];
         let target2 = 3;
         let expected2 = -1;
+        let actual2 = Solution::search(nums2, target2);
+
+        assert_eq!(actual2, expected2);
     }
 }
