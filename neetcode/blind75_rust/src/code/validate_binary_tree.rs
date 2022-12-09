84b4b3d48f0a70ae65eab30e00da350f0fe21058
AndyLu <laserlue@gmail.com>
2022-12-09 09:33:49 +0800

    update validate_binary_tree

--- neetcode/blind75_rust/src/code/validate_binary_tree.rs
+++ neetcode/blind75_rust/src/code/validate_binary_tree.rs
@@ -22,7 +22,54 @@ use std::rc::Rc;
 
 pub struct Solution {}
 
+#[derive(Debug)]
+enum Action<T, U> {
+    Call(T),
+    Handler(U),
+}
+
+type NodeRef<T> = Option<Rc<RefCell<T>>>;
+
 impl Solution {
+    fn is_valid_bst_nonrecur(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        let mut stack = Vec::<Action<(NodeRef<TreeNode>, i64, i64), ()>>::new();
+        let mut ret_stack = Vec::<bool>::new();
+        stack.push(Action::Call((root, i64::MIN, i64::MAX)));
+
+        while let Some(action) = stack.pop() {
+            match action {
+                Action::Call(node) => {
+                    if let (Some(n), min, max) = node {
+                        if n.borrow().val as i64 <= min || n.borrow().val as i64 >= max {
+                            ret_stack.push(false);
+                            continue;
+                        }
+                        stack.push(Action::Handler(()));
+                        stack.push(Action::Call((
+                            n.borrow().right.clone(),
+                            n.borrow().val as i64,
+                            max,
+                        )));
+                        stack.push(Action::Call((
+                            n.borrow().left.clone(),
+                            min,
+                            n.borrow().val as i64,
+                        )));
+                    } else {
+                        ret_stack.push(true);
+                    }
+                }
+                Action::Handler(()) => {
+                    let r1 = ret_stack.pop().unwrap();
+                    let r2 = ret_stack.pop().unwrap();
+                    ret_stack.push(r1 && r2);
+                }
+            }
+        }
+
+        ret_stack.pop().unwrap()
+    }
+
     pub fn is_valid_bst(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
         Solution::is_valid(&root, i64::MIN, i64::MAX)
     }
@@ -48,7 +95,7 @@ mod tests {
     use std::{cell::RefCell, rc::Rc};
 
     #[test]
-    fn case_1() {
+    fn case_recur_1() {
         let root = self::convert_to_tree_bfs(&vec![Some(2), Some(1), Some(3)], 0);
         let expected = true;
         let actual = Solution::is_valid_bst(root);
@@ -57,7 +104,7 @@ mod tests {
     }
 
     #[test]
-    fn case_2() {
+    fn case_recur_2() {
         let root = self::convert_to_tree_bfs(
             &vec![Some(5), Some(1), Some(4), None, None, Some(3), Some(6)],
             0,
@@ -69,7 +116,7 @@ mod tests {
     }
 
     #[test]
-    fn case_3() {
+    fn case_recur_3() {
         let root = self::convert_to_tree_bfs(&vec![Some(i32::MAX)], 0);
         let expected = true;
         let actual = Solution::is_valid_bst(root);
@@ -78,7 +125,7 @@ mod tests {
     }
 
     #[test]
-    fn case_4() {
+    fn case_recur_4() {
         let root = self::convert_to_tree_bfs(&vec![Some(i32::MIN)], 0);
         let expected = true;
         let actual = Solution::is_valid_bst(root);
@@ -87,10 +134,76 @@ mod tests {
     }
 
     #[test]
-    fn case_5() {
+    fn case_recur_5() {
         let root = self::convert_to_tree_bfs(&vec![Some(i32::MAX), Some(i32::MAX)], 0);
         let expected = false;
         let actual = Solution::is_valid_bst(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_recur_6() {
+        let root = self::convert_to_tree_bfs(&vec![Some(0)], 0);
+        let expected = true;
+        let actual = Solution::is_valid_bst(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_nonrecur_1() {
+        let root = self::convert_to_tree_bfs(&vec![Some(2), Some(1), Some(3)], 0);
+        let expected = true;
+        let actual = Solution::is_valid_bst_nonrecur(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_nonrecur_2() {
+        let root = self::convert_to_tree_bfs(
+            &vec![Some(5), Some(1), Some(4), None, None, Some(3), Some(6)],
+            0,
+        );
+        let expected = false;
+        let actual = Solution::is_valid_bst_nonrecur(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_nonrecur_3() {
+        let root = self::convert_to_tree_bfs(&vec![Some(i32::MAX)], 0);
+        let expected = true;
+        let actual = Solution::is_valid_bst_nonrecur(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_nonrecur_4() {
+        let root = self::convert_to_tree_bfs(&vec![Some(i32::MIN)], 0);
+        let expected = true;
+        let actual = Solution::is_valid_bst_nonrecur(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_nonrecur_5() {
+        let root = self::convert_to_tree_bfs(&vec![Some(i32::MAX), Some(i32::MAX)], 0);
+        let expected = false;
+        let actual = Solution::is_valid_bst_nonrecur(root);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_nonrecur_6() {
+        let root = self::convert_to_tree_bfs(&vec![Some(0)], 0);
+        let expected = true;
+        let actual = Solution::is_valid_bst_nonrecur(root);
 
         assert_eq!(expected, actual);
     }
