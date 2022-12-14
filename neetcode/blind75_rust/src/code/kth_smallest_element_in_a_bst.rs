ffb141253c535502bc16010f551d8c0799c01217
AndyLu <laserlue@gmail.com>
2022-12-14 09:20:26 +0800

    feat: update kth_smallest_element_in_a_bst.rs with non-recursion way

--- neetcode/blind75_rust/src/code/kth_smallest_element_in_a_bst.rs
+++ neetcode/blind75_rust/src/code/kth_smallest_element_in_a_bst.rs
@@ -22,7 +22,34 @@ use std::rc::Rc;
 
 pub struct Solution {}
 
+enum Action<T, U> {
+    Caller(T),
+    Handler(U),
+}
+
 impl Solution {
+    pub fn kth_smallest_nonrecur(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
+        let mut stack = Vec::<Action<Option<Rc<RefCell<TreeNode>>>, i32>>::new();
+        let mut ret_stack = Vec::<i32>::new();
+        stack.push(Action::Caller(root));
+        while let Some(action) = stack.pop() {
+            match action {
+                Action::Caller(node) => {
+                    if let Some(n) = node {
+                        stack.push(Action::Caller(n.borrow().right.clone()));
+                        stack.push(Action::Handler(n.borrow().val));
+                        stack.push(Action::Caller(n.borrow().left.clone()));
+                    }
+                }
+                Action::Handler(number) => {
+                    ret_stack.push(number);
+                }
+            }
+        }
+
+        ret_stack[k as usize - 1]
+    }
+
     pub fn kth_smallest(root: Option<Rc<RefCell<TreeNode>>>, k: i32) -> i32 {
         let mut array = Vec::<i32>::new();
         Solution::inorder(&root, &mut array);
@@ -74,6 +101,38 @@ mod tests {
         let k = 3;
         let expected = 3;
         let actual = Solution::kth_smallest(root, k);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_3() {
+        let root = self::convert_to_tree_bfs(&vec![Some(3), Some(1), Some(4), None, Some(2)], 0);
+        let k = 1;
+        let expected = 1;
+        let actual = Solution::kth_smallest_nonrecur(root, k);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn case_4() {
+        let root = self::convert_to_tree_bfs(
+            &vec![
+                Some(5),
+                Some(3),
+                Some(6),
+                Some(2),
+                Some(4),
+                None,
+                None,
+                Some(1),
+            ],
+            0,
+        );
+        let k = 3;
+        let expected = 3;
+        let actual = Solution::kth_smallest_nonrecur(root, k);
 
         assert_eq!(expected, actual);
     }
