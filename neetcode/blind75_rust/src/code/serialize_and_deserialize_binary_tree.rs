cf3e8b8386d07bdb97bff787c6aef031664afbab
AndyLu <laserlue@gmail.com>
2022-12-23 13:38:09 +0800

    feat: serialize_and_deserialize_binary_tree.rs

--- neetcode/blind75_rust/src/code/serialize_and_deserialize_binary_tree.rs
+++ neetcode/blind75_rust/src/code/serialize_and_deserialize_binary_tree.rs
@@ -30,11 +30,42 @@ impl Codec {
     }
 
     fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
-        todo!()
+        let mut ret = String::new();
+        Codec::serialize_helper(root, &mut ret);
+        ret
+    }
+
+    fn serialize_helper(node: Option<Rc<RefCell<TreeNode>>>, s: &mut String) {
+        if let Some(n) = node {
+            s.push_str(&n.borrow().val.to_string());
+            s.push_str(",");
+            Codec::serialize_helper(n.borrow().left.clone(), s);
+            Codec::serialize_helper(n.borrow().right.clone(), s);
+        } else {
+            s.push_str("N");
+            s.push_str(",");
+        }
     }
 
     fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
-        todo!()
+        let w = data.split(",").collect::<Vec<&str>>();
+        let mut i: usize = 0;
+        Codec::deserialize_helper(&mut i, &w)
+    }
+
+    fn deserialize_helper(index: &mut usize, w: &Vec<&str>) -> Option<Rc<RefCell<TreeNode>>> {
+        if w[*index] == "N" {
+            *index += 1;
+            return None;
+        } else {
+            let node = Rc::new(RefCell::new(TreeNode::new(
+                w[*index].parse::<i32>().unwrap(),
+            )));
+            *index += 1;
+            node.borrow_mut().left = Codec::deserialize_helper(index, w);
+            node.borrow_mut().right = Codec::deserialize_helper(index, w);
+            return Some(node);
+        }
     }
 }
 
@@ -52,7 +83,30 @@ mod tests {
 
     #[test]
     fn case_1() {
-        todo!()
+        let strs = self::convert_to_tree_bfs(
+            &vec![Some(1), Some(2), Some(3), None, None, Some(4), Some(5)],
+            0,
+        );
+        let expected = self::convert_to_tree_bfs(
+            &vec![Some(1), Some(2), Some(3), None, None, Some(4), Some(5)],
+            0,
+        );
+        let obj = Codec::new();
+        let data: String = obj.serialize(strs);
+        let ans: Option<Rc<RefCell<TreeNode>>> = obj.deserialize(data);
+
+        assert_eq!(expected, ans);
+    }
+
+    #[test]
+    fn case_2() {
+        let strs = self::convert_to_tree_bfs(&vec![], 0);
+        let expected = self::convert_to_tree_bfs(&vec![], 0);
+        let obj = Codec::new();
+        let data: String = obj.serialize(strs);
+        let ans: Option<Rc<RefCell<TreeNode>>> = obj.deserialize(data);
+
+        assert_eq!(expected, ans);
     }
 
     fn convert_to_tree_bfs(input: &Vec<Option<i32>>, index: i32) -> Option<Rc<RefCell<TreeNode>>> {
