08d2ddb0cbafc89ea8728e9e0289bfe3fe920917
AndyLu <laserlue@gmail.com>
2022-12-23 11:19:17 +0800

    feat: init for serialize_and_deserialize_binary_tree.rs

---
+++ neetcode/blind75_rust/src/code/serialize_and_deserialize_binary_tree.rs
@@ -0,0 +1,71 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+struct Codec {}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Codec {
+    fn new() -> Self {
+        Codec {}
+    }
+
+    fn serialize(&self, root: Option<Rc<RefCell<TreeNode>>>) -> String {
+        todo!()
+    }
+
+    fn deserialize(&self, data: String) -> Option<Rc<RefCell<TreeNode>>> {
+        todo!()
+    }
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * let obj = Codec::new();
+ * let data: String = obj.serialize(strs);
+ * let ans: Option<Rc<RefCell<TreeNode>>> = obj.deserialize(data);
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::{Codec, TreeNode};
+    use std::{cell::RefCell, rc::Rc};
+
+    #[test]
+    fn case_1() {
+        todo!()
+    }
+
+    fn convert_to_tree_bfs(input: &Vec<Option<i32>>, index: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if index > input.len() as i32 - 1 {
+            return None;
+        }
+        if let Some(n) = input[index as usize] {
+            let mut node = TreeNode::new(n);
+            node.left = self::convert_to_tree_bfs(input, 2 * index + 1);
+            node.right = self::convert_to_tree_bfs(input, 2 * index + 2);
+            Some(Rc::new(RefCell::new(node)))
+        } else {
+            None
+        }
+    }
+}
