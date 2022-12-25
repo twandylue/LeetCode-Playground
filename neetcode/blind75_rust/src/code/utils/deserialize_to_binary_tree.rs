7c90f17488ddac7c17135751c0cad5d390169ad2
AndyLu <laserlue@gmail.com>
2022-12-26 07:01:40 +0800

    temp: not finished

--- neetcode/blind75_rust/src/code/utils/deserialize_to_binary_tree.rs
+++ neetcode/blind75_rust/src/code/utils/deserialize_to_binary_tree.rs
@@ -7,32 +7,37 @@ pub fn deserialize_to_BT(vector: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode
         return None;
     }
 
+    println!("{:?}", vector);
+
+    let nodes: Vec<Option<Rc<RefCell<TreeNode>>>> = vector
+        .into_iter()
+        .map(|x| match x {
+            Some(val) => Some(Rc::new(RefCell::new(TreeNode::new(val)))),
+            None => None,
+        })
+        .collect();
+
     let mut index: usize = 0;
     let mut queue: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
+    let root = nodes[index].clone();
+    index += 1;
+    queue.push_back(root);
+    while queue.len() > 0 {
+        if let Some(n) = queue.pop_front().unwrap() {
+            n.borrow_mut().left = nodes[index].clone();
+            index += 1;
+            n.borrow_mut().right = nodes[index].clone();
+            index += 1;
 
-    if let Some(val) = vector[index] {
-        let mut root = Some(Rc::new(RefCell::new(TreeNode::new(val))));
-        queue.push_back(root);
-        index += 1;
-        while queue.len() > 0 {
-            // let mut node = queue.pop_front().unwrap();
-            if let Some(n) = queue.pop_front().unwrap() {
-                match vector[index] {
-                    Some(v) => {
-                        n.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(v))));
-                        index += 1;
-                        n.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(v))));
-                        index += 1;
-                    }
-                    None => {
-                        todo!()
-                    }
-                }
+            if n.borrow().left != None {
+                queue.push_back(n.borrow_mut().left.clone());
+            }
+
+            if n.borrow().right != None {
+                queue.push_back(n.borrow_mut().right.clone());
             }
         }
-
-        return root.clone();
-    } else {
-        return None;
     }
+
+    return None;
 }
