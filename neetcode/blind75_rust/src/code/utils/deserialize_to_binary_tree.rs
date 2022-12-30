ab511129fe5aa076d76b8e0da10e0eeba9a56047
AndyLu <laserlue@gmail.com>
2022-12-30 22:34:55 +0800

    refactor: modify deserialize_to_binary_tree.rs

--- neetcode/blind75_rust/src/code/utils/deserialize_to_binary_tree.rs
+++ neetcode/blind75_rust/src/code/utils/deserialize_to_binary_tree.rs
@@ -1,13 +1,11 @@
 use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
-use crate::code::tree::binary_tree_node::TreeNode;
+use crate::code::Model::binary_tree_node::TreeNode;
 
 pub fn deserialize_to_BT(vector: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
     if vector.len() == 0 {
         return None;
     }
-
-    println!("{:?}", vector);
 
     let nodes: Vec<Option<Rc<RefCell<TreeNode>>>> = vector
         .into_iter()
@@ -25,23 +23,26 @@ pub fn deserialize_to_BT(vector: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode
     queue.push_back(root);
     while queue.len() > 0 {
         if let Some(n) = &*(queue.pop_front()).unwrap() {
-            if index == nodes.len() {
+            if let Some(v) = nodes.get(index) {
+                n.borrow_mut().left = v.clone();
+                index += 1;
+            } else {
                 break;
             }
-            n.borrow_mut().left = nodes[index].clone();
-            index += 1;
-            if index == nodes.len() {
+
+            if let Some(v) = nodes.get(index) {
+                n.borrow_mut().right = v.clone();
+                index += 1;
+            } else {
                 break;
             }
-            n.borrow_mut().right = nodes[index].clone();
-            index += 1;
 
             if n.borrow().left != None {
-                queue.push_back(Rc::new(n.borrow_mut().left.clone()));
+                queue.push_back(Rc::new(n.borrow().left.clone()));
             }
 
             if n.borrow().right != None {
-                queue.push_back(Rc::new(n.borrow_mut().right.clone()));
+                queue.push_back(Rc::new(n.borrow().right.clone()));
             }
         }
     }
