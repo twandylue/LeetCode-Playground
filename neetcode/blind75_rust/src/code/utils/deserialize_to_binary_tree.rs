af7ee6c5bdfb53aa965ac71494879ee274d2c834
AndyLu <laserlue@gmail.com>
2022-12-30 20:16:27 +0800

    feat: add deserialize_to_binary_tree.rs for deserializing a binary search tree in iterative way.

--- neetcode/blind75_rust/src/code/utils/deserialize_to_binary_tree.rs
+++ neetcode/blind75_rust/src/code/utils/deserialize_to_binary_tree.rs
@@ -18,26 +18,33 @@ pub fn deserialize_to_BT(vector: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode
         .collect();
 
     let mut index: usize = 0;
-    let mut queue: VecDeque<Option<Rc<RefCell<TreeNode>>>> = VecDeque::new();
-    let root = nodes[index].clone();
+    let mut queue: VecDeque<Rc<Option<Rc<RefCell<TreeNode>>>>> = VecDeque::new();
+    let root = Rc::new(nodes[index].clone());
+    let head = Rc::clone(&root);
     index += 1;
     queue.push_back(root);
     while queue.len() > 0 {
-        if let Some(n) = queue.pop_front().unwrap() {
+        if let Some(n) = &*(queue.pop_front()).unwrap() {
+            if index == nodes.len() {
+                break;
+            }
             n.borrow_mut().left = nodes[index].clone();
             index += 1;
+            if index == nodes.len() {
+                break;
+            }
             n.borrow_mut().right = nodes[index].clone();
             index += 1;
 
             if n.borrow().left != None {
-                queue.push_back(n.borrow_mut().left.clone());
+                queue.push_back(Rc::new(n.borrow_mut().left.clone()));
             }
 
             if n.borrow().right != None {
-                queue.push_back(n.borrow_mut().right.clone());
+                queue.push_back(Rc::new(n.borrow_mut().right.clone()));
             }
         }
     }
 
-    return None;
+    return (*head).clone();
 }
