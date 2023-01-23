dfd6d9f959fed020e056a450b6ababbb7dece9b4
AndyLu <laserlue@gmail.com>
2023-01-23 09:17:59 +0800

    feat: add counting_bits.rs(TODO)

---
+++ neetcode/blind75_rust/src/code/counting_bits.rs
@@ -0,0 +1,28 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn count_bits(n: i32) -> Vec<i32> {
+        todo!()
+    }
+}
+
+// #[cfg(test)]
+// mod test {
+//     use super::Solution;
+//
+//     #[test]
+//     fn case_1() {
+//         let n = 2;
+//         let expected = vec![0, 1, 1];
+//         let actual = Solution::count_bits(n);
+//         assert_eq!(expected, actual);
+//     }
+//
+//     #[test]
+//     fn case_2() {
+//         let n = 5;
+//         let expected = vec![0, 1, 1, 2, 1, 2];
+//         let actual = Solution::count_bits(n);
+//         assert_eq!(expected, actual);
+//     }
+// }
