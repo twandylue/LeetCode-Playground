7efccdefd142a25133222fba63be0efb2f4a3f15
AndyLu <laserlue@gmail.com>
2023-01-29 11:31:17 +0800

    feat: add reverse_bits.rs (TODO)

---
+++ neetcode/blind75_rust/src/code/reverse_bits.rs
@@ -0,0 +1,29 @@
+pub struct Solution {}
+impl Solution {
+    pub fn reverse_bits(x: u32) -> u32 {
+        todo!();
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::Solution;
+//
+//     #[test]
+//     fn case_1() {
+//         let n = 0b00000010100101000001111010011100;
+//         let expected = 964176192;
+//         let actual = Solution::reverse_bits(n);
+//
+//         assert_eq!(expected, actual);
+//     }
+//
+//     #[test]
+//     fn case_2() {
+//         let n = 0b11111111111111111111111111111101;
+//         let expected = 3221225471;
+//         let actual = Solution::reverse_bits(n);
+//
+//         assert_eq!(expected, actual);
+//     }
+// }
