b88f3ad0d690a016b2331a745e96404fdea0ec16
AndyLu <laserlue@gmail.com>
2023-06-03 10:49:57 +0800

    feat: trapping rain water
    New method in LC. 42. Trapping Rain Water.

--- python/trapping_rain_water.py
+++ python/trapping_rain_water.py
@@ -1,6 +1,6 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        output = 0;
+        output = 0
         max_lefts: list[int] = []
         max_rights: list[int] = []
 
@@ -21,14 +21,35 @@ class Solution:
             val: int = min(max_lefts[i], new_max_rights[i]) - height[i]
             if val > 0:
                 output += val
-            else: 
+            else:
                 continue
 
-        return output;
+        return output
+
+    def trap_2(self, height: list[int]) -> int:
+        output: int = 0
+        max_left: int = height[0]
+        max_right: int = height[-1]
+        l: int = 0
+        r: int = len(height) - 1
+        while l < r and l < len(height) and r > 0:
+            if height[l] <= height[r]:
+                max_left = max(max_left, height[l])
+                if max_left - height[l] > 0:
+                    output += max_left - height[l]
+                l += 1
+            else:
+                max_right = max(max_right, height[r])
+                if max_right - height[r] > 0:
+                    output += max_right - height[r]
+                r -= 1
+
+        return output
+
 
 def test_trap_case_1():
     # arrange
-    height: list[int] = [0,1,0,2,1,0,1,3,2,1,2,1]
+    height: list[int] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
     expected: int = 6
 
     # act
@@ -38,14 +59,41 @@ def test_trap_case_1():
     # assert
     assert actual == expected
 
+
 def test_trap_case_2():
     # arrange
-    height: list[int] = [4,2,0,3,2,5]
+    height: list[int] = [4, 2, 0, 3, 2, 5]
     expected: int = 9
 
     # act
     solution = Solution()
     actual = solution.trap(height)
+
+    # assert
+    assert actual == expected
+
+
+def test_trap_case_3():
+    # arrange
+    height: list[int] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+    expected: int = 6
+
+    # act
+    solution = Solution()
+    actual = solution.trap_2(height)
+
+    # assert
+    assert actual == expected
+
+
+def test_trap_case_4():
+    # arrange
+    height: list[int] = [4, 2, 0, 3, 2, 5]
+    expected: int = 9
+
+    # act
+    solution = Solution()
+    actual = solution.trap_2(height)
 
     # assert
     assert actual == expected
--- rust/src/code/trapping_rain_water.rs
+++ rust/src/code/trapping_rain_water.rs
@@ -40,6 +40,32 @@ impl Solution {
 
         return result;
     }
+
+    pub fn trap_2(height: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let mut max_left = height[0];
+        let mut max_right = height[height.len() - 1];
+        let mut l = 0;
+        let mut r = height.len() - 1;
+
+        while l < r && r > 0 && l < height.len() {
+            if height[l] <= height[r] {
+                max_left = std::cmp::max(height[l], max_left);
+                if max_left - height[l] > 0 {
+                    result += (max_left - height[l]);
+                }
+                l += 1;
+            } else {
+                max_right = std::cmp::max(height[r], max_right);
+                if max_right - height[r] > 0 {
+                    result += (max_right - height[r]);
+                }
+                r -= 1;
+            }
+        }
+
+        return result;
+    }
 }
 
 #[cfg(test)]
@@ -67,6 +93,32 @@ mod test {
 
         //  act
         let actual = Solution::trap(height);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn trapping_rain_water_case_3() {
+        // arrange
+        let height = vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+        let expected = 6;
+
+        //  act
+        let actual = Solution::trap_2(height);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn trapping_rain_water_case_4() {
+        // arrange
+        let height = vec![4, 2, 0, 3, 2, 5];
+        let expected = 9;
+
+        //  act
+        let actual = Solution::trap_2(height);
 
         // assert
         assert_eq!(expected, actual);
