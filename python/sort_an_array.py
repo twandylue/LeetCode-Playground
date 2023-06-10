9cca901c0da84333ab57db39b7dcefe86413862c
AndyLu <laserlue@gmail.com>
2023-06-11 07:16:37 +0800

    feat: sort an array(merged sorting)
    LC. 912. Sort an Array

---
+++ python/sort_an_array.py
@@ -0,0 +1,285 @@
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        output: list[int] = self.sortArrayHelper(nums)
+
+        return output
+
+    def sortArrayHelper(self, nums: list[int]) -> list[int]:
+        if len(nums) == 2:
+            if nums[0] > nums[1]:
+                return [nums[1], nums[0]]
+            else:
+                return nums
+        elif len(nums) == 1:
+            return nums
+
+        mid: int = int(len(nums) / 2)
+        left: list[int] = self.sortArrayHelper(nums[:mid])
+        right: list[int] = self.sortArrayHelper(nums[mid:])
+
+        return self.merge(left, right)
+
+    def merge(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        m: int = len(nums1)
+        n: int = len(nums2)
+        index: int = m + n - 1
+        nums = [0] * (m + n)
+
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums[index] = nums1[m - 1]
+                m -= 1
+            else:
+                nums[index] = nums2[n - 1]
+                n -= 1
+            if index > 0:
+                index -= 1
+
+        while m > 0 or n > 0:
+            if n > 0:
+                nums[index] = nums2[n - 1]
+                n -= 1
+            if m > 0:
+                nums[index] = nums1[m - 1]
+                m -= 1
+            if index > 0:
+                index -= 1
+
+        return nums
+
+
+def test_sort_an_array_case_1():
+    # arrange
+    numbers: list[int] = [5, 2, 3, 1]
+    expected: list[int] = [1, 2, 3, 5]
+
+    # act
+    solution = Solution()
+    actual: list[int] = solution.sortArray(numbers)
+
+    # assert
+    assert actual == expected
+
+
+def test_sort_an_array_case_2():
+    # arrange
+    numbers: list[int] = [5, 1, 1, 2, 0, 0]
+    expected: list[int] = [0, 0, 1, 1, 2, 5]
+
+    # act
+    solution = Solution()
+    actual: list[int] = solution.sortArray(numbers)
+
+    # assert
+    assert actual == expected
+
+
+def test_sort_an_array_case_3():
+    # arrange
+    numbers: list[int] = [
+        -74,
+        48,
+        -20,
+        2,
+        10,
+        -84,
+        -5,
+        -9,
+        11,
+        -24,
+        -91,
+        2,
+        -71,
+        64,
+        63,
+        80,
+        28,
+        -30,
+        -58,
+        -11,
+        -44,
+        -87,
+        -22,
+        54,
+        -74,
+        -10,
+        -55,
+        -28,
+        -46,
+        29,
+        10,
+        50,
+        -72,
+        34,
+        26,
+        25,
+        8,
+        51,
+        13,
+        30,
+        35,
+        -8,
+        50,
+        65,
+        -6,
+        16,
+        -2,
+        21,
+        -78,
+        35,
+        -13,
+        14,
+        23,
+        -3,
+        26,
+        -90,
+        86,
+        25,
+        -56,
+        91,
+        -13,
+        92,
+        -25,
+        37,
+        57,
+        -20,
+        -69,
+        98,
+        95,
+        45,
+        47,
+        29,
+        86,
+        -28,
+        73,
+        -44,
+        -46,
+        65,
+        -84,
+        -96,
+        -24,
+        -12,
+        72,
+        -68,
+        93,
+        57,
+        92,
+        52,
+        -45,
+        -2,
+        85,
+        -63,
+        56,
+        55,
+        12,
+        -85,
+        77,
+        -39,
+    ]
+    expected: list[int] = [
+        -96,
+        -91,
+        -90,
+        -87,
+        -85,
+        -84,
+        -84,
+        -78,
+        -74,
+        -74,
+        -72,
+        -71,
+        -69,
+        -68,
+        -63,
+        -58,
+        -56,
+        -55,
+        -46,
+        -46,
+        -45,
+        -44,
+        -44,
+        -39,
+        -30,
+        -28,
+        -28,
+        -25,
+        -24,
+        -24,
+        -22,
+        -20,
+        -20,
+        -13,
+        -13,
+        -12,
+        -11,
+        -10,
+        -9,
+        -8,
+        -6,
+        -5,
+        -3,
+        -2,
+        -2,
+        2,
+        2,
+        8,
+        10,
+        10,
+        11,
+        12,
+        13,
+        14,
+        16,
+        21,
+        23,
+        25,
+        25,
+        26,
+        26,
+        28,
+        29,
+        29,
+        30,
+        34,
+        35,
+        35,
+        37,
+        45,
+        47,
+        48,
+        50,
+        50,
+        51,
+        52,
+        54,
+        55,
+        56,
+        57,
+        57,
+        63,
+        64,
+        65,
+        65,
+        72,
+        73,
+        77,
+        80,
+        85,
+        86,
+        86,
+        91,
+        92,
+        92,
+        93,
+        95,
+        98,
+    ]
+
+    # act
+    solution = Solution()
+    actual: list[int] = solution.sortArray(numbers)
+
+    # assert
+    assert actual == expected
--- rust/src/code/sort_an_array.rs
+++ rust/src/code/sort_an_array.rs
@@ -2,48 +2,66 @@ struct Solution {}
 
 impl Solution {
     fn sort_array(nums: Vec<i32>) -> Vec<i32> {
-        let mut output = nums.clone();
-        Self::quick_sort_helper(0, nums.len() - 1, &mut output);
+        let output = Self::merge_sort_helper(nums);
 
         return output;
     }
 
-    fn quick_sort_helper(l: usize, r: usize, nums: &mut Vec<i32>) {
-        todo!();
-        // if l < r && r < nums.len() {
-        //     let pivot = r;
-        //     if pivot == 0 {
-        //         return;
-        //     }
+    // merge sorting
+    fn merge_sort_helper(nums: Vec<i32>) -> Vec<i32> {
+        if nums.len() == 2 {
+            if nums[0] > nums[1] {
+                return vec![nums[1], nums[0]];
+            } else {
+                return nums;
+            }
+        } else if nums.len() == 1 {
+            return nums;
+        }
 
-        //     Self::partition(l, r - 1, pivot, nums);
-        //     Self::quick_sort_helper(l, pivot - 1, nums);
-        //     Self::quick_sort_helper(pivot + 1, r, nums);
-        // }
+        let mid = nums.len() / 2;
+        let left = Self::merge_sort_helper(nums[..mid].to_vec());
+        let right = Self::merge_sort_helper(nums[mid..].to_vec());
+
+        let sorted_array = Self::merge(left, right);
+
+        return sorted_array;
     }
 
-    fn partition(mut l: usize, mut r: usize, pivot: usize, nums: &mut Vec<i32>) {
-        while l < r && r < nums.len() && r > 0 {
-            if nums[l] <= nums[pivot] {
-                l += 1;
-            } else if nums[l] > nums[pivot] && nums[r] > nums[pivot] {
-                r -= 1;
-            } else if nums[l] > nums[pivot] && nums[r] <= nums[pivot] {
-                Self::swap(l, r, nums);
-                l += 1;
-                r -= 1;
+    fn merge(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut m = nums1.len();
+        let mut n = nums2.len();
+        let mut index = m + n - 1;
+        let mut merged_vec: Vec<i32> = vec![0; m + n];
+
+        while m > 0 && n > 0 {
+            if nums1[m - 1] > nums2[n - 1] {
+                merged_vec[index] = nums1[m - 1];
+                m -= 1;
+            } else {
+                merged_vec[index] = nums2[n - 1];
+                n -= 1;
+            }
+            if index > 0 {
+                index -= 1;
             }
         }
 
-        if nums[l] > nums[pivot] {
-            Self::swap(pivot, l, nums);
-        }
-    }
+        while m > 0 || n > 0 {
+            if n > 0 {
+                merged_vec[index] = nums2[n - 1];
+                n -= 1;
+            } else if m > 0 {
+                merged_vec[index] = nums1[m - 1];
+                m -= 1;
+            }
 
-    fn swap(l: usize, r: usize, nums: &mut Vec<i32>) {
-        let temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
+            if index > 0 {
+                index -= 1;
+            }
+        }
+
+        return merged_vec;
     }
 }
 
@@ -52,7 +70,6 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn sort_array_case_1() {
         // arrange
         let nums = vec![5, 2, 3, 1];
@@ -66,7 +83,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn sort_array_case_2() {
         // arrange
         let nums = vec![5, 1, 1, 2, 0, 0];
@@ -74,6 +90,44 @@ mod tests {
 
         // act
         let actual = Solution::sort_array(nums);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn sort_array_case_3() {
+        // arrange
+        let input = Vec::from([
+            -74, 48, -20, 2, 10, -84, -5, -9, 11, -24, -91, 2, -71, 64, 63, 80, 28, -30, -58, -11,
+            -44, -87, -22, 54, -74, -10, -55, -28, -46, 29, 10, 50, -72, 34, 26, 25, 8, 51, 13, 30,
+            35, -8, 50, 65, -6, 16, -2, 21, -78, 35, -13, 14, 23, -3, 26, -90, 86, 25, -56, 91,
+            -13, 92, -25, 37, 57, -20, -69, 98, 95, 45, 47, 29, 86, -28, 73, -44, -46, 65, -84,
+            -96, -24, -12, 72, -68, 93, 57, 92, 52, -45, -2, 85, -63, 56, 55, 12, -85, 77, -39,
+        ]);
+        let expected = vec![
+            -96, -91, -90, -87, -85, -84, -84, -78, -74, -74, -72, -71, -69, -68, -63, -58, -56,
+            -55, -46, -46, -45, -44, -44, -39, -30, -28, -28, -25, -24, -24, -22, -20, -20, -13,
+            -13, -12, -11, -10, -9, -8, -6, -5, -3, -2, -2, 2, 2, 8, 10, 10, 11, 12, 13, 14, 16,
+            21, 23, 25, 25, 26, 26, 28, 29, 29, 30, 34, 35, 35, 37, 45, 47, 48, 50, 50, 51, 52, 54,
+            55, 56, 57, 57, 63, 64, 65, 65, 72, 73, 77, 80, 85, 86, 86, 91, 92, 92, 93, 95, 98,
+        ];
+
+        // act
+        let actual = Solution::sort_array(input);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn sort_array_case_4() {
+        // arrange
+        let input = Vec::from([4, 3, 2, 1]);
+        let expected = vec![1, 2, 3, 4];
+
+        // act
+        let actual = Solution::sort_array(input);
 
         // assert
         assert_eq!(expected, actual);
