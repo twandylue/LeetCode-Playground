0feb24fad180bac018b279fd738c24b8e2ceecc3
AndyLu <laserlue@gmail.com>
2023-06-11 19:51:07 +0800

    feat: search a 2D matrix
    Add a new method.

--- python/search_a_2d_matrix.py
+++ python/search_a_2d_matrix.py
@@ -17,6 +17,41 @@ class Solution:
 
         return False
 
+    def searchMatrix2(self, matrix: list[list[int]], target: int) -> bool:
+        l: int = 0
+        r: int = len(matrix[0]) - 1
+        bot: int = 0
+        top: int = len(matrix) - 1
+        while bot <= top:
+            row: int = (bot + top) // 2
+            if target > matrix[row][r]:
+                bot = row + 1
+            elif target < matrix[row][l]:
+                if row > 0:
+                    top = row - 1
+                else:
+                    break
+            else:
+                break
+
+        if bot > top:
+            return False
+        row: int = (bot + top) // 2
+
+        while l <= r:
+            mid: int = (l + r) // 2
+            if target == matrix[row][mid]:
+                return True
+            elif target < matrix[row][mid]:
+                if mid > 0:
+                    r = mid - 1
+                else:
+                    break
+            elif target > matrix[row][mid]:
+                l = mid + 1
+
+        return False
+
 
 def test_searchMatrix_case_1():
     # arrange
@@ -55,6 +90,48 @@ def test_searchMatrix_case_3():
     # act
     solution = Solution()
     actual = solution.searchMatrix(matrix, target)
+
+    # assert
+    assert actual == expected
+
+
+def test_searchMatrix_case_4():
+    # arrange
+    matrix: list[list[int]] = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+    target: int = 3
+    expected: bool = True
+
+    # act
+    solution = Solution()
+    actual = solution.searchMatrix2(matrix, target)
+
+    # assert
+    assert actual == expected
+
+
+def test_searchMatrix_case_5():
+    # arrange
+    matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+    target = 13
+    expected = False
+
+    # act
+    solution = Solution()
+    actual = solution.searchMatrix2(matrix, target)
+
+    # assert
+    assert actual == expected
+
+
+def test_searchMatrix_case_6():
+    # arrange
+    matrix = [[1, 3]]
+    target = 3
+    expected = True
+
+    # act
+    solution = Solution()
+    actual = solution.searchMatrix2(matrix, target)
 
     # assert
     assert actual == expected
--- rust/src/code/search_a_2d_matrix.rs
+++ rust/src/code/search_a_2d_matrix.rs
@@ -24,6 +24,50 @@ impl Solution {
 
         false
     }
+
+    pub fn search_matrix2(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        let mut bot = 0;
+        let mut top = matrix.len() - 1;
+        let mut l = 0;
+        let mut r = matrix[0].len() - 1;
+
+        while bot <= top {
+            let mid = (bot + top) / 2;
+            if target > matrix[mid][r] {
+                bot = mid + 1;
+            } else if target < matrix[mid][0] {
+                if mid > 0 {
+                    top = mid - 1;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        if bot > top {
+            return false;
+        }
+        let row = (bot + top) / 2;
+
+        while l <= r {
+            let mid = (l + r) / 2;
+            if target == matrix[row][mid] {
+                return true;
+            } else if target > matrix[row][mid] {
+                l = mid + 1;
+            } else if target < matrix[row][mid] {
+                if mid > 0 {
+                    r = mid - 1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return false;
+    }
 }
 
 #[cfg(test)]
@@ -81,6 +125,90 @@ mod tests {
 
         // act
         let actual = Solution::search_matrix(matrix, target);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_matrix_case_5() {
+        // arrange
+        let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
+        let target = 3;
+        let expected = true;
+
+        // act
+        let actual = Solution::search_matrix2(matrix, target);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_matrix_case_6() {
+        // arrange
+        let matrix = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
+        let target = 13;
+        let expected = false;
+
+        // act
+        let actual = Solution::search_matrix2(matrix, target);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_matrix_case_7() {
+        // arrange
+        let matrix = vec![vec![0]];
+        let target = 0;
+        let expected = true;
+
+        // act
+        let actual = Solution::search_matrix2(matrix, target);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_matrix_case_8() {
+        // arrange
+        let matrix = vec![vec![1]];
+        let target = 0;
+        let expected = false;
+
+        // act
+        let actual = Solution::search_matrix2(matrix, target);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_matrix_case_9() {
+        // arrange
+        let matrix = vec![vec![1]];
+        let target = 2;
+        let expected = false;
+
+        // act
+        let actual = Solution::search_matrix2(matrix, target);
+
+        // assert
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn search_matrix_case_10() {
+        // arrange
+        let matrix = vec![vec![1], vec![3], vec![5]];
+        let target = 3;
+        let expected = true;
+
+        // act
+        let actual = Solution::search_matrix2(matrix, target);
 
         // assert
         assert_eq!(expected, actual);
