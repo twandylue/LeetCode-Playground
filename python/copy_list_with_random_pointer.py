d395200681cbc8f6398d4e1939ef6db2ec0c134e
AndyLu <laserlue@gmail.com>
2023-07-08 15:03:48 +0800

    feat(python): LC. 138. Copy List with Random Pointer
    LC. 138. Copy List with Random Pointer

---
+++ python/copy_list_with_random_pointer.py
@@ -0,0 +1,43 @@
+from typing import Optional
+import sys
+
+sys.path.append("./models")
+# from list_node import ListNode
+
+sys.path.append("./utils")
+from linkedListConverter import convert_to_linked_list
+from linkedListConverter import convert_linked_list_to_list
+
+
+class Node:
+    def __init__(
+        self, x: int, next: Optional["Node"] = None, random: Optional["Node"] = None
+    ):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        if head is None:
+            return None
+        dic: dict[Node, Node] = dict()
+        pointer: Optional[Node] = head
+        newHead: Optional[Node] = Node(0)
+        newPointer: Optional[Node] = newHead
+        while pointer is not None:
+            node: Node = Node(pointer.val, pointer.next, None)
+            dic[pointer] = node
+            pointer = pointer.next
+
+            newPointer.next = node
+            newPointer = newPointer.next
+
+        pointer = head
+        while pointer is not None:
+            if pointer.random is not None:
+                dic[pointer].random = dic[pointer.random]
+            pointer = pointer.next
+
+        return newHead.next
