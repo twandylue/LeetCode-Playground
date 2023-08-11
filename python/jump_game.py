856af23c06b2e55eca902dd939bed4cbcc4ca236
AndyLu <laserlue@gmail.com>
2023-08-11 09:22:11 -0600

    feat(python): LC. 55. Jump Game
    LC. 55. Jump Game

---
+++ python/jump_game.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        goal: int = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return True if goal == 0 else False
+
+
+def test_canJump_case_1():
+    # arrange
+    expected: bool = True
+    nums: list[int] = [2, 3, 1, 1, 4]
+
+    # act
+    actual: bool = Solution().canJump(nums)
+
+    # assert
+    assert expected == actual
+
+
+def test_canJump_case_2():
+    # arrange
+    nums: list[int] = [3, 2, 1, 0, 4]
+    expected: bool = False
+
+    # act
+    actual: bool = Solution().canJump(nums)
+
+    # assert
+    assert expected == actual
