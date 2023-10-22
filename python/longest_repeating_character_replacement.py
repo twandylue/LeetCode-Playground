b7cb7f6d2f2577115597cfa5431effb100c60cae
AndyLu <laserlue@gmail.com>
2023-10-22 09:07:02 -0600

    feat(python): LC 424. Longest Repeating Character Replacement
    LC 424. Longest Repeating Character Replacement

---
+++ python/longest_repeating_character_replacement.py
@@ -0,0 +1,77 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        result: int = 0
+        strArr: list[str] = [0] * 26
+        currLen: int = 0
+        l: int = 0
+        r: int = 0
+        while l < len(s) and r < len(s):
+            strArr[ord(s[r]) - ord("A")] += 1
+            currLen = r - l + 1
+            while currLen - max(strArr) > k:
+                strArr[ord(s[l]) - ord("A")] -= 1
+                l += 1
+                currLen -= 1
+            r += 1
+
+            result = max(result, currLen)
+
+        result = max(result, currLen)
+
+        return result
+
+
+def test_characterReplacement_case_1():
+    # arrange
+    s: str = "ABAB"
+    k: str = 2
+    expected: int = 4
+
+    # act
+    solution = Solution()
+    actual = solution.characterReplacement(s, k)
+
+    # assert
+    assert expected == actual
+
+
+def test_characterReplacement_case_2():
+    # arrange
+    s: str = "AABABBA"
+    k: str = 1
+    expected: int = 4
+
+    # act
+    solution = Solution()
+    actual = solution.characterReplacement(s, k)
+
+    # assert
+    assert expected == actual
+
+
+def test_characterReplacement_case_3():
+    # arrange
+    s: str = "BAAA"
+    k: str = 0
+    expected: int = 3
+
+    # act
+    solution = Solution()
+    actual = solution.characterReplacement(s, k)
+
+    # assert
+    assert expected == actual
+
+
+def test_characterReplacement_case_4():
+    # arrange
+    s: str = "ABBB"
+    k: str = 2
+    expected: int = 4
+
+    # act
+    solution = Solution()
+    actual = solution.characterReplacement(s, k)
+
+    # assert
+    assert expected == actual
