125b7bcbf7f4ca5b6e8bda185cbe42c69d46dc9f
AndyLu <laserlue@gmail.com>
2023-11-06 00:47:56 -0700

    feat(python): LC. 287. Find the Duplicate Number
    LC. 287. Find the Duplicate Number

---
+++ python/find_the_duplicate_number.py
@@ -0,0 +1,34 @@
+# TODO:
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        d: dict[int, int] = dict()
+        for n in nums:
+            if n in d:
+                return n
+            d[n] = 1
+        return 0
+
+def test_findDuplicate_case_1():
+    # arrange
+    nums: list[int] = [1,3,4,2,2]
+    expected: int = 2
+
+    # act
+    solution = Solution()
+    actual = solution.findDuplicate(nums)
+
+    # assert
+    assert expected == actual
+
+
+def test_findDuplicate_case_2():
+    # arrange
+    nums: list[int] = [3,1,3,4,2]
+    expected: int = 3
+
+    # act
+    solution = Solution()
+    actual = solution.findDuplicate(nums)
+
+    # assert
+    assert expected == actual
