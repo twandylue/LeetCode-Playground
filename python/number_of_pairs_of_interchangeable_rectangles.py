942c6a9e79bc6587d748df99321e12092cc75ce9
AndyLu <laserlue@gmail.com>
2023-11-29 23:47:07 -0700

    feat(python): LC. 2001. Number of Pairs of Interchangeable Rectangles
    LC. 2001. Number of Pairs of Interchangeable Rectangles# Please enter the commit message for your changes. Lines starting

---
+++ python/number_of_pairs_of_interchangeable_rectangles.py
@@ -0,0 +1,35 @@
+import math
+
+
+class Solution:
+    def interchangeableRectangles(self, rectangles: list[list[int]]) -> int:
+        ratioMap: dict[float, int] = dict()
+        count: int = 0
+
+        for rectangle in rectangles:
+            if rectangle[0] / rectangle[1] not in ratioMap:
+                ratioMap[rectangle[0] / rectangle[1]] = 1
+            else:
+                ratioMap[rectangle[0] / rectangle[1]] += 1
+
+        print(ratioMap)
+        for _, v in ratioMap.items():
+            if v > 1:
+                count += int(
+                    math.factorial(v) / (math.factorial(2) * math.factorial(v - 2))
+                )
+
+        return count
+
+
+def test_interchangeableRectangles_case_1():
+    # arrange
+    rectangles: list[list[int]] = [[4, 8], [3, 6], [10, 20], [15, 30]]
+    expected: int = 6
+
+    # act
+    solution = Solution()
+    actual = solution.interchangeableRectangles(rectangles)
+
+    # assert
+    assert expected == actual
