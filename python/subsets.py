2ac1b87710b5467de750798222f186a70437a335
AndyLu <laserlue@gmail.com>
2023-12-16 12:26:32 +0800

    feat(python): use bit mask in subsets
    Use bit mask to git subsets.

--- python/subsets.py
+++ python/subsets.py
@@ -1,4 +1,17 @@
 class Solution:
+    def subsets_2(self, nums: list[int]) -> list[list[int]]:
+        n: int = len(nums)
+        result: list[list[int]] = [[]]
+
+        for mask in range(1, 1 << n):
+            subSet: list[int] = list()
+            for i in range(n):
+                if mask & (1 << i):
+                    subSet.append(nums[i])
+            result.append(subSet)
+
+        return result
+
     def subsets(self, nums: list[int]) -> list[list[int]]:
         result: list[list[int]] = []
         subset: list[int] = []
@@ -43,6 +56,21 @@ def test_subsets_case_2():
     # act
     solution = Solution()
     actual = solution.subsets(nums)
+
+    # assert
+    expected.sort()
+    actual.sort()
+    assert expected == actual
+
+
+def test_subsets_case_3():
+    # arrange
+    nums: list[int] = [1, 2]
+    expected: list[list[int]] = [[], [1], [2], [1, 2]]
+
+    # act
+    solution = Solution()
+    actual = solution.subsets_2(nums)
 
     # assert
     expected.sort()
