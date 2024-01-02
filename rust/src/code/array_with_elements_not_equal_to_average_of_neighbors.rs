7b1c9cfbafe2ab07ad3104017fc8c4a5d3e7cd56
AndyLu <laserlue@gmail.com>
2024-01-03 00:13:46 +0800

    refactor: LC. 1968. Array With Elements Not Equal to Average of Neighbors
    LC. 1968. Array With Elements Not Equal to Average of Neighbors

--- rust/src/code/array_with_elements_not_equal_to_average_of_neighbors.rs
+++ rust/src/code/array_with_elements_not_equal_to_average_of_neighbors.rs
@@ -4,21 +4,15 @@ impl Solution {
     // NOTE: time complexity: O(nlogn)
     pub fn rearrange_array(mut nums: Vec<i32>) -> Vec<i32> {
         nums.sort();
-        let mut result: Vec<i32> = vec![0; nums.len()];
-        let h: usize = if nums.len() % 2 == 0 {
-            nums.len() / 2
-        } else {
-            (nums.len() + 1) / 2
-        };
+        let mut result: Vec<i32> = Vec::new();
         let mut l: usize = 0;
-        let mut r: usize = h;
-        for i in 0..result.len() {
-            if i % 2 == 0 && l < h {
-                result[i] = nums[l];
-                l += 1;
-            } else if i % 2 > 0 && r < nums.len() {
-                result[i] = nums[r];
-                r += 1;
+        let mut r: usize = nums.len() - 1;
+        while result.len() != nums.len() {
+            result.push(nums[l]);
+            l += 1;
+            if l < r {
+                result.push(nums[r]);
+                r -= 1;
             }
         }
 
