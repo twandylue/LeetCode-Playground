dc96444a66d1e9ae2b327a06ec6d6678d6f89dc9
AndyLu <laserlue@gmail.com>
2024-01-04 08:10:12 +0800

    refactor: LC. 75. Sort Colors
    LC. 75. Sort Colors

--- python/sort_colors.py
+++ python/sort_colors.py
@@ -3,25 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # [0,2,1,2]
         l: int = 0
-        m: int = 0
+        i: int = 0
         r: int = len(nums) - 1
-        while m <= r:
-            if nums[m] == 0:
-                tmp: int = nums[m]
-                nums[m] = nums[l]
-                nums[l] = tmp
+        while i <= r:
+            if nums[i] == 0:
+                nums[l], nums[i] = nums[i], nums[l]
                 l += 1
-                # m += 1
-                m = l
-            elif nums[m] == 2:
-                tmp: int = nums[m]
-                nums[m] = nums[r]
-                nums[r] = tmp
+                i += 1
+            elif nums[i] == 2:
+                nums[r], nums[i] = nums[i], nums[r]
                 r -= 1
             else:
-                m += 1
+                i += 1
 
 
 def test_sortColors_case_1():
