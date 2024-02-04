30c7f79131b309b86e73a851d8245b66a3f55283
AndyLu <laserlue@gmail.com>
2024-02-04 08:38:13 -0700

    feat(python): LC. 876. Middle of the Linked List
    LC. 876. Middle of the Linked List

---
+++ python/middle_of_the_linked_list.py
@@ -0,0 +1,52 @@
+"""module to reorder the linked list"""
+import sys
+
+sys.path.append("./models")
+sys.path.append("./utils")
+
+from typing import Optional
+from list_node import ListNode
+from linkedListConverter import convert_to_linked_list
+from assert_two_linked_list import assert_two_linked_list
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast: Optional[ListNode] = head
+        slow: Optional[ListNode] = head
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
+
+
+def test_middleNode_case_1():
+    # arrange
+    head: list[int] = [1, 2, 3, 4, 5]
+    expected: list[int] = [3, 4, 5]
+
+    # act
+    solution = Solution()
+    actual = solution.middleNode(convert_to_linked_list(head))
+
+    # assert
+    assert assert_two_linked_list(convert_to_linked_list(expected), actual) == True
+
+
+def test_middleNode_case_2():
+    # arrange
+    head: list[int] = [1, 2, 3, 4, 5, 6]
+    expected: list[int] = [4, 5, 6]
+
+    # act
+    solution = Solution()
+    actual = solution.middleNode(convert_to_linked_list(head))
+
+    # assert
+    assert assert_two_linked_list(convert_to_linked_list(expected), actual) == True
