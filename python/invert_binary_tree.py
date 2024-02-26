ba03f4ac057df21bca7e692f70fb1addd5146b56
AndyLu <laserlue@gmail.com>
2024-02-26 05:44:50 -0700

    refactor(python): LC. 226. Invert Binary Tree
    LC. 226. Invert Binary Tree

--- python/invert_binary_tree.py
+++ python/invert_binary_tree.py
@@ -1,24 +1,27 @@
-from typing import Optional
+"""module to reorder the linked list"""
+
 import sys
 
 sys.path.append("./models")
-from binary_tree_node import TreeNode
-
 sys.path.append("./utils")
+
+from typing import Optional
+from binary_tree_node import TreeNode
 from deserialize_to_binary_tree import DeserializeFromList
 from serialize_binary_tree import SerializeBinaryTreeToList
 
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root == None:
+        if root is None:
             return None
 
         return self.dfs(root)
 
-    def dfs(self, root) -> Optional[TreeNode]:
-        if root == None:
+    def dfs(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
             return None
+
         self.dfs(root.left)
         self.dfs(root.right)
         root.left, root.right = root.right, root.left
@@ -28,11 +31,11 @@ class Solution:
 def test_invertTree_case_1():
     # arrange
     root: Optional[TreeNode] = DeserializeFromList([4, 2, 7, 1, 3, 6, 9])
-    if root == None:
+    if root is None:
         raise Exception("failed")
 
     expected: Optional[TreeNode] = DeserializeFromList([4, 7, 2, 9, 6, 3, 1])
-    if expected == None:
+    if expected is None:
         raise Exception("failed")
 
     # act
