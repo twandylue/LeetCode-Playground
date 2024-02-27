815d3e0e9afd277c0edca03b45ddbda99b13e2bf
AndyLu <laserlue@gmail.com>
2024-02-27 09:42:30 -0700

    refactor(python): LC. 543. Diameter of Binary Tree
    LC. 543. Diameter of Binary Tree

--- python/diameter_of_binary_tree.py
+++ python/diameter_of_binary_tree.py
@@ -1,37 +1,39 @@
-from typing import Optional
 import sys
 
 sys.path.append("./models")
-from binary_tree_node import TreeNode
-
 sys.path.append("./utils")
+
+from typing import Optional
+from binary_tree_node import TreeNode
 from deserialize_to_binary_tree import DeserializeFromList
 from serialize_binary_tree import SerializeBinaryTreeToList
 
 
 class Solution:
-    maxDis: int
-
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.maxDis: int = 0
-        self.helper(root, self.maxDis)
-        return self.maxDis
-
-    def helper(self, root: Optional[TreeNode], maxDis: int) -> int:
-        if root == None:
+        """time complexity: O(n), this problem is to find out the "longest path"""
+        #  NOTE: hint
+        # |path rooted at node|= height(left)+height(right)
+        # height(node)=1+max(height(left), height(right))
+        if root is None:
             return 0
-        left = self.helper(root.left, maxDis)
-        right = self.helper(root.right, maxDis)
-        self.maxDis = max(left + right, maxDis)
+        return self.dfs(root.left) + self.dfs(root.right)
 
-        return max(left, right) + 1
+    def dfs(self, root: Optional[TreeNode]) -> int:
+        """postorder traversal"""
+        if root is None:
+            return 0
+        left: int = self.dfs(root.left)
+        right: int = self.dfs(root.right)
+
+        return 1 + max(left, right)
 
 
 def test_diameterOfBinaryTree_case_1():
     # arrange
     arr: list[int] = [1, 2, 3, 4, 5]
     root: Optional[TreeNode] = DeserializeFromList(arr)
-    if root == None:
+    if root is None:
         raise Exception("failed")
     expected: int = 3
 
@@ -47,7 +49,7 @@ def test_diameterOfBinaryTree_case_2():
     # arrange
     arr: list[int] = [1, 2]
     root: Optional[TreeNode] = DeserializeFromList(arr)
-    if root == None:
+    if root is None:
         raise Exception("failed")
     expected: int = 1
 
