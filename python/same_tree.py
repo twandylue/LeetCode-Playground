184afc3d39dcaf609d18d3ad83a1b462f4515f6f
AndyLu <laserlue@gmail.com>
2024-02-28 08:55:12 -0700

    refactor(python): LC. 100. Same Tree
    LC. 100. Same Tree

--- python/same_tree.py
+++ python/same_tree.py
@@ -1,10 +1,10 @@
-from typing import Optional
 import sys
 
 sys.path.append("./models")
-from binary_tree_node import TreeNode
-
 sys.path.append("./utils")
+
+from typing import Optional
+from binary_tree_node import TreeNode
 from deserialize_to_binary_tree import DeserializeFromList
 from serialize_binary_tree import SerializeBinaryTreeToList
 
@@ -17,47 +17,34 @@ from serialize_binary_tree import SerializeBinaryTreeToList
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p == None and q == None:
+        """time complexity: O(n)"""
+        return self.dfs(p, q)
+
+    def dfs(self, p: Optional[TreeNode], q: list[Optional[TreeNode]]) -> bool:
+        """preorder traversal"""
+        if p is None and q is None:
             return True
 
-        pResult: list[Optional[TreeNode]] = self.dfs(p, list())
-        qResult: list[Optional[TreeNode]] = self.dfs(q, list())
-
-        if len(qResult) != len(pResult):
+        if p is None and q is not None:
             return False
-        for i in range(0, len(qResult)):
-            if qResult[i] is None and pResult[i] is None:
-                continue
-            if (qResult[i] is None and pResult[i] is not None) or (
-                qResult[i] is not None and pResult[i] is None
-            ):
-                return False
-            if qResult[i].val != pResult[i].val:
-                return False
-        return True
 
-    def dfs(
-        self, n: Optional[TreeNode], v: list[Optional[TreeNode]]
-    ) -> list[Optional[TreeNode]]:
-        if n is None:
-            v.append(None)
-            return v
+        if p is not None and q is None:
+            return False
 
-        v.append(n)
-        self.dfs(n.left, v)
-        self.dfs(n.right, v)
+        if p.val != q.val:
+            return False
 
-        return v
+        return self.dfs(p.right, q.right) and self.dfs(p.left, q.left)
 
 
 def test_isSameTree_case_1():
     # arrange
     p: Optional[TreeNode] = DeserializeFromList([1, 2, 3])
-    if p == None:
+    if p is None:
         raise Exception("failed")
 
     q: Optional[TreeNode] = DeserializeFromList([1, 2, 3])
-    if q == None:
+    if q is None:
         raise Exception("failed")
 
     # act
@@ -71,11 +58,11 @@ def test_isSameTree_case_1():
 def test_isSameTree_case_2():
     # arrange
     p: Optional[TreeNode] = DeserializeFromList([0])
-    if p == None:
+    if p is None:
         raise Exception("failed")
 
     q: Optional[TreeNode] = DeserializeFromList([1])
-    if q == None:
+    if q is None:
         raise Exception("failed")
 
     # act
