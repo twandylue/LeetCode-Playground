44422229ae646300a1094a6b8c8320aef4f17c90
AndyLu <laserlue@gmail.com>
2024-03-09 10:10:40 -0700

    format(c): add clang-format
    Add clang-format to follow the rule of linux kernel coding style.

--- c/src/remove_linked_list_elements.c
+++ c/src/remove_linked_list_elements.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 struct ListNode {
-  int val;
-  struct ListNode *next;
+	int val;
+	struct ListNode *next;
 };
 
-struct ListNode *removeElements(struct ListNode *head, int val) {
-  if (head == NULL) {
-    return NULL;
-  }
-  struct ListNode **indirect = &head;
-  while (*indirect != NULL) {
-    if ((*indirect)->val == val) {
-      *indirect = (*indirect)->next;
-    } else {
-      indirect = &(*indirect)->next;
-    }
-  }
-  return head;
+struct ListNode *removeElements(struct ListNode *head, int val)
+{
+	if (head == NULL) {
+		return NULL;
+	}
+	struct ListNode **indirect = &head;
+	while (*indirect != NULL) {
+		if ((*indirect)->val == val) {
+			*indirect = (*indirect)->next;
+		} else {
+			indirect = &(*indirect)->next;
+		}
+	}
+	return head;
 }
 
-int main(void) { return 0; }
+int main(void)
+{
+	return 0;
+}
