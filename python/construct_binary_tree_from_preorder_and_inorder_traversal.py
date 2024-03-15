7da7941b9cf0b30c7e90f3b6329980548df7b997
AndyLu <laserlue@gmail.com>
2024-03-15 08:22:37 -0600

    feat(python): LC. 105. Construct Binary Tree from Preorder and Inorder Traversal
    LC. 105. Construct Binary Tree from Preorder and Inorder Traversal

--- python/construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ python/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("./models")
 sys.path.append("./utils")
 
+from collections import deque
 from typing import Optional
 from binary_tree_node import TreeNode
 from deserialize_to_binary_tree import DeserializeFromList
@@ -16,41 +17,60 @@ from serialize_binary_tree import SerializeBinaryTreeToList
 #         self.left = left
 #         self.right = right
 class Solution:
-    def buildTree(self, inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
+    def buildTree(self, preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
         """time complexity: O(n)"""
+        preorder_que: deque[int] = deque(preorder)
         inorder_map: dict[int, int] = {}
         for i in range(len(inorder)):
             inorder_map[inorder[i]] = i
-        return self.build_helper(0, len(inorder) - 1, inorder, postorder, inorder_map)
+
+        return self.build_helper(
+            0, len(inorder) - 1, preorder_que, inorder, inorder_map
+        )
 
     def build_helper(
         self,
         l: int,
         r: int,
+        preorder: deque[int],
         inorder: list[int],
-        postorder: list[int],
         inorder_map: dict[int, int],
     ) -> Optional[TreeNode]:
         """helper function for buildTree. It's not a dfs, just a recursive function."""
         if l > r:
             return None
-        root: TreeNode = TreeNode(postorder.pop())
+        root: TreeNode = TreeNode(preorder.popleft())
         idx: int = inorder_map[root.val]
-        root.right = self.build_helper(idx + 1, r, inorder, postorder, inorder_map)
-        root.left = self.build_helper(l, idx - 1, inorder, postorder, inorder_map)
+        root.left = self.build_helper(l, idx - 1, preorder, inorder, inorder_map)
+        root.right = self.build_helper(idx + 1, r, preorder, inorder, inorder_map)
         return root
 
 
 def test_buildTree_case_1():
     """This is a test case"""
     # arrange
+    preorder: list[int] = [3, 9, 20, 15, 7]
     inorder: list[int] = [9, 3, 15, 20, 7]
-    postorder: list[int] = [9, 15, 7, 20, 3]
     expected: Optional[TreeNode] = DeserializeFromList([3, 9, 20, None, None, 15, 7])
 
     # act
     solution = Solution()
-    actual = solution.buildTree(inorder, postorder)
+    actual = solution.buildTree(preorder, inorder)
+
+    # assert
+    assert SerializeBinaryTreeToList(expected) == SerializeBinaryTreeToList(actual)
+
+
+def test_buildTree_case_2():
+    """This is a test case"""
+    # arrange
+    preorder: list[int] = [-1]
+    inorder: list[int] = [-1]
+    expected: Optional[TreeNode] = DeserializeFromList([-1])
+
+    # act
+    solution = Solution()
+    actual = solution.buildTree(preorder, inorder)
 
     # assert
     assert SerializeBinaryTreeToList(expected) == SerializeBinaryTreeToList(actual)
