08ec32546c333cf0864e4865659f136e15901e6f
AndyLu <laserlue@gmail.com>
2024-12-31 11:12:57 +0800

    refactor(python): LC. 739. Daily Temperatures
    LC. 739. Daily Temperatures

--- python/daily_temperatures.py
+++ python/daily_temperatures.py
@@ -1,16 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
-        output: list[int] = [0] * len(temperatures)
-        stack: list[int] = []
-
-        for i in range(len(temperatures)):
-            while len(stack) > 0 and temperatures[stack[-1]] < temperatures[i]:
-                output[stack[-1]] = i - stack[-1]
-                stack.pop()
-
-            stack.append(i)
-
-        return output
+        """time complexity: O(n)"""
+        result: list[int] = [0] * len(temperatures)
+        stack: list[tuple[int, int]] = []
+        for i, t in enumerate(temperatures):
+            while len(stack) > 0 and t > stack[-1][0]:
+                _, idx = stack.pop()
+                result[idx] = i - idx
+            stack.append((t, i))
+        return result
 
 
 def test_dailyTemperatures_case_1():
