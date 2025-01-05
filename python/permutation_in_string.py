cdeb51a7e6ea86530887017bf440ecff89997ade
AndyLu <laserlue@gmail.com>
2025-01-05 19:22:05 +0800

    refactor(python): Refactor
    Refactor

--- python/permutation_in_string.py
+++ python/permutation_in_string.py
@@ -1,39 +1,34 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        """time complexity: O(n)"""
         if len(s1) > len(s2):
             return False
-
-        matches: int = 0
-        s1Count: list[int] = [0] * 26
-        s2Count: list[int] = [0] * 26
-
+        s1_count: list[int] = [0] * 26
+        s2_count: list[int] = [0] * 26
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] += 1
-            s2Count[ord(s2[i]) - ord("a")] += 1
-
+            s1_count[ord(s1[i]) - ord("a")] += 1
+            s2_count[ord(s2[i]) - ord("a")] += 1
+        matches: int = 0
         for i in range(26):
-            matches += 1 if s1Count[i] == s2Count[i] else 0
-
-        s: int = 0
-        for e in range(len(s1), len(s2)):
+            if s1_count[i] == s2_count[i]:
+                matches += 1
+        l: int = 0
+        for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
-
-            index: int = ord(s2[e]) - ord("a")
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
+            index: int = ord(s2[r]) - ord("a")
+            s2_count[index] += 1
+            if s1_count[index] == s2_count[index]:
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif s1_count[index] + 1 == s2_count[index]:
                 matches -= 1
-
-            index = ord(s2[s]) - ord("a")
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
+            index = ord(s2[l]) - ord("a")
+            s2_count[index] -= 1
+            if s1_count[index] == s2_count[index]:
                 matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
+            elif s1_count[index] - 1 == s2_count[index]:
                 matches -= 1
-            s += 1
-
+            l += 1
         return matches == 26
 
 
