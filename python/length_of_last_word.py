22ca34a688ded4361acbb0273f5e0a16c4f47a15
AndyLu <laserlue@gmail.com>
2025-01-08 18:21:17 -0800

    refactor(python): LC. 58. Length of Last Word
    LC. 58. Length of Last Word

--- python/length_of_last_word.py
+++ python/length_of_last_word.py
@@ -1,17 +1,18 @@
 class Solution:
     # time complexity: O(n)
     def lengthOfLastWord(self, s: str) -> int:
+        """time complexity: O(n)"""
         r: int = len(s) - 1
         result: int = 0
         while r >= 0:
-            if not s[r] == " ":
-                l: int = r
-                while l >= 0 and not s[l] == " ":
-                    result += 1
-                    l -= 1
-                break
-            r -= 1
-
+            if s[r] == " ":
+                r -= 1
+                continue
+            l: int = r
+            while s >= 0 and s[l] != " ":
+                l -= 1
+                result += 1
+            break
         return result
 
 
