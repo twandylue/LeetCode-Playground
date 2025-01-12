c2dd71625148e885321b39710060294bc42b4cf9
AndyLu <laserlue@gmail.com>
2025-01-11 17:50:49 -0700

    refactor(python): LC. 155. Min Stack
    LC. 155. Min Stack

--- python/min_stack.py
+++ python/min_stack.py
@@ -1,29 +1,29 @@
 class MinStack:
+
     def __init__(self):
-        self.stack: list[int] = []
-        self.min_stack: list[int] = []
+        self._stack: list[int] = []
+        self._min_stack: list[int] = []
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if len(self.min_stack) == 0:
-            self.min_stack.append(val)
-        elif val < self.min_stack[-1]:
-            self.min_stack.append(val)
+        """time complexity: O(1), space complexity: O(n)"""
+        self._stack.append(val)
+        if len(self._min_stack) > 0 and val > self._min_stack[-1]:
+            self._min_stack.append(self._min_stack[-1])
         else:
-            self.min_stack.append(self.min_stack[-1])
-
-        return None
+            self._min_stack.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
-        self.min_stack.pop()
-        return None
+        """time complexity: O(1), space complexity: O(n)"""
+        self._stack.pop()
+        self._min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        """time complexity: O(1)"""
+        return self._stack[-1]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        """time complexity: O(1)"""
+        return self._min_stack[-1]
 
 
 def test_min_stack_case_1():
@@ -68,3 +68,24 @@ def test_min_stack_case_2():
     assert actual1 == expected1
     assert actual2 == expected2
     assert actual3 == expected3
+
+
+def test_min_stack_case_3():
+    # arrange
+    obj = MinStack()
+    expected1 = -3
+    expected2 = -3
+
+    # act
+    obj.push(-2)
+    obj.push(-2)
+    obj.push(-3)
+    obj.push(-3)
+    # [-2, -2, -3, -3]
+    actual1: int = obj.getMin()
+    obj.pop()
+    actual2: int = obj.getMin()
+
+    # assert
+    assert actual1 == expected1
+    assert actual2 == expected2
