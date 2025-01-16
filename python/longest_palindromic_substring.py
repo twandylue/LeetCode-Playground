279b0ec5d084583c55d71fe9f5e734e110171188
AndyLu <laserlue@gmail.com>
2025-01-15 19:41:13 -0700

    refactor(python): LC. 5. Longest Palindromic Substring
    LC. 5. Longest Palindromic Substring

--- python/longest_palindromic_substring.py
+++ python/longest_palindromic_substring.py
@@ -1,33 +1,43 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        result: str = ""
-        resultLen: int = 0
+        """time complexity: O(n^2)"""
+        idx: int = 0
+        length: int = 0
         for i in range(len(s)):
+            # for odd
             l: int = i
             r: int = i
-            while r < len(s) and s[l] == s[r]:
-                if r - l + 1 > resultLen:
-                    resultLen = r - l + 1
-                    result = s[l : r + 1]
-                if l > 0:
-                    l -= 1
-                    r += 1
-                else:
-                    break
-
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > length:
+                    length = r - l + 1
+                    idx = l
+                l -= 1
+                r += 1
+            # for even
             l: int = i
             r: int = i + 1
-            while r < len(s) and s[l] == s[r]:
-                if r - l + 1 > resultLen:
-                    resultLen = r - l + 1
-                    result = s[l : r + 1]
-                if l > 0:
-                    l -= 1
-                    r += 1
-                else:
-                    break
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > length:
+                    length = r - l + 1
+                    idx = l
+                l -= 1
+                r += 1
+        return s[idx : idx + length]
 
-        return result
+    def longestPalindrome2(self, s: str) -> str:
+        """time complexity: O(n^2)"""
+        n: int = len(s)
+        dp: list[list[bool]] = [[False] * n for _ in range(n)]
+        idx: int = 0
+        length: int = 0
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    if length < (j - i + 1):
+                        length = j - i + 1
+                        idx = i
+        return s[idx : idx + length]
 
 
 def test_longestPalindrome_case_1():
