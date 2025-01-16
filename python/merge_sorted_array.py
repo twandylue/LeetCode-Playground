757be42d185b3a087c9791b44205aa47814bbda5
AndyLu <laserlue@gmail.com>
2025-01-15 17:57:48 -0700

    refactor(python): LC. 88. Merge Sorted Array
    LC. 88. Merge Sorted Array

--- python/merge_sorted_array.py
+++ python/merge_sorted_array.py
@@ -3,28 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        last: int = len(nums1) - 1
-        r1: int = m - 1
-        r2: int = n - 1
-
-        while r1 >= 0 and r2 >= 0 and last >= 0:
-            if nums1[r1] > nums2[r2]:
-                nums1[last] = nums1[r1]
-                r1 -= 1
-            else:
-                nums1[last] = nums2[r2]
-                r2 -= 1
-            last -= 1
-
-        while r1 >= 0 and last >= 0:
-            nums1[last] = nums1[r1]
-            r1 -= 1
-            last -= 1
-
-        while r2 >= 0 and last >= 0:
-            nums1[last] = nums2[r2]
-            r2 -= 1
-            last -= 1
+        i: int = m - 1
+        j: int = n - 1
+        k: int = len(nums1) - 1
+        while k >= 0 and (i >= 0 or j >= 0):
+            if i >= 0 and j >= 0:
+                if nums1[i] > nums2[j]:
+                    nums1[k] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[k] = nums2[j]
+                    j -= 1
+            elif i >= 0:
+                nums1[k] = nums1[i]
+                i -= 1
+            elif j >= 0:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
 
 
 def test_merge_sorted_array_case_1():
