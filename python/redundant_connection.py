b34bb1e2f44eddf5f2ae606bc688d4177c64f0ee
AndyLu <laserlue@gmail.com>
2025-03-28 15:46:10 -0600

    refactor(python): LC. 684. Redundant Connection
    LC. 684. Redundant Connection

--- python/redundant_connection.py
+++ python/redundant_connection.py
@@ -1,31 +1,31 @@
-class DSU:
+class UnionFind:
     def __init__(self, n: int):
+        self._ranks: list[int] = [1] * (n + 1)
         self._parents: list[int] = [i for i in range(n + 1)]
-        self._rank: list[int] = [1] * (n + 1)
 
-    def find(self, node: int) -> int:
-        while node != self._parents[node]:
-            node = self._parents[node]
-        return node
+    def find(self, n: int) -> int:
+        while n != self._parents[n]:
+            n = self._parents[n]
+        return n
 
     def union(self, u: int, v: int) -> bool:
-        ru: int = self.find(u)
-        rv: int = self.find(v)
-        if ru == rv:
+        pu: int = self.find(u)
+        pv: int = self.find(v)
+        if pu == pv:
             return False
-        if self._rank[ru] < self._rank[rv]:
-            ru, rv = rv, ru
-        self._rank[ru] += self._rank[rv]
-        self._parents[rv] = ru
+        if self._ranks[pv] > self._ranks[pu]:
+            pu, pv = pv, pu
+        self._ranks[pu] += self._ranks[pv]
+        self._parents[pv] = pu
         return True
 
 
 class Solution:
     def findRedundantConnection(self, edges: list[list[int]]) -> list[int]:
         """time complexity: O(E + a(V)), where E is the number of edges and V is the number of vertices"""
-        dsu: DSU = DSU(len(edges))
+        uf: UnionFind = UnionFind(len(edges))
         for u, v in edges:
-            if not dsu.union(u, v):
+            if not uf.union(u, v):
                 return [u, v]
         return []
 
