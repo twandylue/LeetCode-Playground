85df0e2e9151766de6a9ae707fb665fac8758ae9
AndyLu <laserlue@gmail.com>
2025-04-09 07:28:38 -0600

    refactor(python): LC. 567. Permutation in String
    LC. 567. Permutation in String

--- python/permutation_in_string.py
+++ python/permutation_in_string.py
@@ -3,32 +3,32 @@ class Solution:
         """time complexity: O(n)"""
         if len(s1) > len(s2):
             return False
+        matches: int = 0
         s1_count: list[int] = [0] * 26
         s2_count: list[int] = [0] * 26
         for i in range(len(s1)):
             s1_count[ord(s1[i]) - ord("a")] += 1
             s2_count[ord(s2[i]) - ord("a")] += 1
-        matches: int = 0
-        for i in range(26):
+        for i in range(len(s1_count)):
             if s1_count[i] == s2_count[i]:
                 matches += 1
         l: int = 0
         for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
-            index: int = ord(s2[r]) - ord("a")
-            s2_count[index] += 1
-            if s1_count[index] == s2_count[index]:
+            l_index: int = ord(s2[l]) - ord("a")
+            s2_count[l_index] -= 1
+            if s2_count[l_index] == s1_count[l_index]:
                 matches += 1
-            elif s1_count[index] + 1 == s2_count[index]:
-                matches -= 1
-            index = ord(s2[l]) - ord("a")
-            s2_count[index] -= 1
-            if s1_count[index] == s2_count[index]:
-                matches += 1
-            elif s1_count[index] - 1 == s2_count[index]:
+            elif s2_count[l_index] + 1 == s1_count[l_index]:
                 matches -= 1
             l += 1
+            r_index: int = ord(s2[r]) - ord("a")
+            s2_count[r_index] += 1
+            if s2_count[r_index] == s1_count[r_index]:
+                matches += 1
+            elif s2_count[r_index] - 1 == s1_count[r_index]:
+                matches -= 1
         return matches == 26
 
 
