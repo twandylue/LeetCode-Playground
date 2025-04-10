783a7427e392eda6f0cb49d703e171038e1cf6bd
AndyLu <laserlue@gmail.com>
2025-04-09 19:58:13 -0600

    refactor(python): LC. 19. Remove Nth Node From End of List
    LC. 19. Remove Nth Node From End of List

--- python/remove_nth_node_from_end_of_list.py
+++ python/remove_nth_node_from_end_of_list.py
@@ -10,42 +10,22 @@ from linkedListConverter import convert_linked_list_to_list
 
 
 class Solution:
-    # NOTE: time complexity: O(n)
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if head is None:
-            return None
-
-        dummy: ListNode = ListNode()
+        """time complexity: O(n)"""
+        dummy: Optional[ListNode] = ListNode()
         dummy.next = head
-        fast: Optional[ListNode] = dummy.next
         slow: Optional[ListNode] = dummy
-        while slow is not None and fast is not None:
-            if n <= 0:
-                slow = slow.next
+        fast: Optional[ListNode] = head
+        for _ in range(n):
+            if fast is None:
+                break
             fast = fast.next
-            n -= 1
-        slow.next = slow.next.next
-
+        while slow is not None and fast is not None:
+            slow = slow.next
+            fast = fast.next
+        if slow is not None and slow.next is not None:
+            slow.next = slow.next.next
         return dummy.next
-
-    # def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-    #     if head is None:
-    #         return None
-    #
-    #     dummy: ListNode = ListNode()
-    #     dummy.next = head
-    #     fast: Optional[ListNode] = head
-    #     for _ in range(n):
-    #         fast = fast.next
-    #
-    #     slow: Optional[ListNode] = dummy
-    #     while fast is not None:
-    #         fast = fast.next
-    #         slow = slow.next
-    #
-    #     slow.next = slow.next.next
-    #
-    #     return dummy.next
 
 
 def test_removeNthFromEnd_case_1():
