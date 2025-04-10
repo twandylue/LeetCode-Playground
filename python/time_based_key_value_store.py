bb646e4512e73b809c95651c67281805fb175178
AndyLu <laserlue@gmail.com>
2025-04-09 19:19:24 -0600

    refactor(python): LC. 981. Time Based Key-Value Store
    LC. 981. Time Based Key-Value Store

--- python/time_based_key_value_store.py
+++ python/time_based_key_value_store.py
@@ -1,37 +1,27 @@
+from collections import defaultdict
+
+
 class TimeMap:
     def __init__(self):
-        self.store: dict[str, list[tuple[str, int]]] = dict()
+        self._store: dict[str, list[tuple[str, int]]] = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = [(value, timestamp)]
-        else:
-            self.store[key].append((value, timestamp))
+        """time complexity: O(1)"""
+        self._store[key].append((value, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store:
-            return ""
-
-        pos: int = self.binarySearch(self.store[key], timestamp)
-        if pos == -1:
-            return ""
-
-        return self.store[key][pos][0]
-
-    def binarySearch(self, times: list[tuple[str, int]], time: int) -> int:
+        """time complexity: O(lon g)"""
+        data: list[tuple[str, int]] = self._store.get(key, [])
         l: int = 0
-        r: int = len(times) - 1
-        result: int = -1
+        r: int = len(data) - 1
+        result: str = ""
         while l <= r:
             mid: int = (l + r) // 2
-            if times[mid][1] == time:
-                return mid
-            if times[mid][1] < time:
+            if data[mid][1] <= timestamp:
+                result = data[mid][0]
                 l = mid + 1
-                result = mid
             else:
                 r = mid - 1
-
         return result
 
 
