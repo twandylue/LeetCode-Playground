7a2b4d96deebd4cabfeafd0808372a36e4498f12
AndyLu <laserlue@gmail.com>
2025-04-11 22:09:34 -0600

    refactor(python): LC. 994. Rotting Oranges
    LC. 994. Rotting Oranges

--- python/rotting_oranges.py
+++ python/rotting_oranges.py
@@ -4,39 +4,49 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: list[list[int]]) -> int:
         """time complexity: O(m * n)"""
-        time: int = 0
-        fresh: int = 0
         visited: set[tuple[int, int]] = set()
         queue: deque[tuple[int, int]] = deque()
+        fresh: list[int] = [0]
         for r in range(len(grid)):
             for c in range(len(grid[r])):
                 if grid[r][c] == 1:
-                    fresh += 1
+                    fresh[0] = fresh[0] + 1
                 if grid[r][c] == 2:
                     queue.append((r, c))
                     visited.add((r, c))
-        directions: list[tuple[int, int]] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        while len(queue) > 0 and fresh > 0:
+        time: int = 0
+        while len(queue) > 0 and fresh[0] > 0:
             for _ in range(len(queue)):
                 row, col = queue.popleft()
-                for dr, dc in directions:
-                    r: int = row + dr
-                    c: int = col + dc
-                    if (
-                        r < 0
-                        or r >= len(grid)
-                        or c < 0
-                        or c >= len(grid[r])
-                        or (r, c) in visited
-                        or grid[r][c] == 0
-                    ):
-                        continue
-                    visited.add((r, c))
-                    queue.append((r, c))
-                    grid[r][c] = 2
-                    fresh -= 1
+                self.add_cell(row + 1, col, visited, queue, fresh, grid)
+                self.add_cell(row - 1, col, visited, queue, fresh, grid)
+                self.add_cell(row, col + 1, visited, queue, fresh, grid)
+                self.add_cell(row, col - 1, visited, queue, fresh, grid)
             time += 1
-        return time if fresh == 0 else -1
+        return time if fresh[0] == 0 else -1
+
+    def add_cell(
+        self,
+        r: int,
+        c: int,
+        visited: set[tuple[int, int]],
+        queue: deque[tuple[int, int]],
+        fresh: list[int],
+        grid: list[list[int]],
+    ) -> None:
+        if (
+            r < 0
+            or r == len(grid)
+            or c < 0
+            or c == len(grid[r])
+            or (r, c) in visited
+            or grid[r][c] == 0
+        ):
+            return
+        visited.add((r, c))
+        queue.append((r, c))
+        grid[r][c] = 2
+        fresh[0] -= 1
 
 
 def test_orangesRotting_case_1():
