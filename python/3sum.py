96e14db30a4c38011c9e359d4d41f7cb12e93324
AndyLu <laserlue@gmail.com>
2025-07-11 15:26:31 -0600

    refactor(python): LC. 15. 3Sum
    LC. 15. 3Sum

--- python/3sum.py
+++ python/3sum.py
@@ -45,6 +45,31 @@ class Solution:
 
         return result
 
+    def threeSum3(self, nums: list[int]) -> list[list[int]]:
+        result: list[list[int]] = list()
+        nums.sort()
+
+        prev_i: int = -float("inf")
+        for i in range(0, len(nums) - 2):
+            if prev_i == nums[i]:
+                continue
+            j: int = i + 1
+            k: int = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    result.append([nums[i], nums[j], nums[k]])
+                    prev_j = nums[j]
+                    while prev_j == nums[j] and j < k:
+                        j += 1
+
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+            prev_i = nums[i]
+
+        return result
+
 
 def test_threeSum_case_1():
     # arrange
@@ -115,6 +140,22 @@ def test_threeSum_case_5():
     # act
     solution = Solution()
     actual = solution.threeSum(nums)
+
+    # assert
+    expected.sort()
+    actual.sort()
+    assert expected == actual
+
+
+def test_threeSum_case_6():
+    # arrange
+    nums: list[int] = [-2, 0, 1, 1, 2]
+
+    expected: list[list[int]] = [[-2, 0, 2], [-2, 1, 1]]
+
+    # act
+    solution = Solution()
+    actual = solution.threeSum3(nums)
 
     # assert
     expected.sort()
