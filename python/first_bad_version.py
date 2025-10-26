bc30c2b31152a9cdc5044cbe1a0baa9253ae8b6e
AndyLu <laserlue@gmail.com>
2025-10-26 09:55:31 -0600

    feat: LC. 278. First Bad Version

---
+++ python/first_bad_version.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l: int = 1
+        r: int = n
+        while l < r:
+            mid: int = l + (r - l) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
