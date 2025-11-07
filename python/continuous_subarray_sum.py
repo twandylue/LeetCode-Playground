d5cf0426803a97754ff9cc4f7acaf758edbf54dd
AndyLu <laserlue@gmail.com>
2025-11-06 23:47:14 -0700

    refactor: LC. 523. Continuous Subarray Sum

--- python/continuous_subarray_sum.py
+++ python/continuous_subarray_sum.py
@@ -12,6 +12,17 @@ class Solution:
                 return True
         return False
 
+    def checkSubarraySum2(self, nums: list[int], k: int) -> bool:
+        prefix_sum: int = 0
+        prefix_map: dict[int, int] = {0: -1}
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            if prefix_sum % k in prefix_map and i - prefix_map[prefix_sum % k] >= 2:
+                return True
+            if prefix_sum % k not in prefix_map:
+                prefix_map[prefix_sum % k] = i
+        return False
+
 
 def test_checkSubarraySum_case_1():
     # arrange
@@ -34,6 +45,19 @@ def test_checkSubarraySum_case_2():
 
     # act
     actual = Solution().checkSubarraySum(nums, k)
+
+    # assert
+    assert actual == expected
+
+
+def test_checkSubarraySum_case_3():
+    # arrange
+    nums: list = [23, 2, 6, 4, 7]
+    k: int = 13
+    expected: bool = False
+
+    # act
+    actual = Solution().checkSubarraySum2(nums, k)
 
     # assert
     assert actual == expected
